e154e7f6d75c1da62e27c991aa0a5d8d0653b1ec
Christian Ruigrok <christian@ruigrok.info>
2018-06-20 10:49:26 +0200

    Improve formatting

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,15 +1,13 @@
 import fs from 'fs';
 
-const boats = JSON.parse(fs.readFileSync("./data/boats.json"));
-
-//let nextId = channels.length;
+const boats = JSON.parse(fs.readFileSync('./data/boats.json'));
 
 export const resolvers = {
-    Query: {
-        boats: () => boats.items,
-        columns: () => boats.columns
-    }
-    /*
+  Query: {
+    boats: () => boats.items,
+    columns: () => boats.columns
+  }
+  /*
     Mutation: {
         addChannel: (root, args) => {
             const newChannel = { id: ++nextId, name: args.name };
--- src/schema.js
+++ src/schema.js
@@ -2,38 +2,36 @@ import { makeExecutableSchema } from 'graphql-tools';
 import { resolvers } from './resolvers';
 
 const typeDefs = `
+	type Column {
+		type: String
+		name: String
+		extend: Boolean
+	}
 
-type Column {
-	type: String
-	name: String
-	extend: Boolean
-}
+	type Item {
+		id: Int!
+		name: String
+		type: String
+		crew: String
+		rented: [Rented]
+		repairs: [Repair]
+	}
 
-type Item {
-	id: Int!
-	name: String
-	type: String
-	crew: String
-	rented: [Rented]
-	repairs: [Repair]
-}
+	type Rented {
+		date: String
+		from: String
+		to: String
+		by: String
+	}
 
-type Rented {
-	date: String
-	from: String
-	to: String
-	by: String
-}
+	type Repair {
+		info: String
+	}
 
-type Repair {
-	info: String
-}
-
-type Query {
-	columns: [Column]
-	boats: [Item]
-}
-
+	type Query {
+		columns: [Column]
+		boats: [Item]
+	}
 `;
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
